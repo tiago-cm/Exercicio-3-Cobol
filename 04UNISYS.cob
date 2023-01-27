2a2d311b310410b22a62434ec6d7bee83256b0a2
Tiago Moraes <tiago.costa.moraes@outlook.com>
2023-01-27 06:41:03 -0300

    Sintaxe melhorada

--- 04UNISYS.cob
+++ 04UNISYS.cob
@@ -1,26 +1,20 @@
-      ******************************************************************
-      * Author:
-      * Date:
-      * Purpose:
-      * Tectonics: cobc
-      ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. 04UNISYS.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01 DATA-INICIAL.
-         03 DIA-INICIAL  PIC 9(02).
-         03 MES-INICIAL PIC 9(02).
-         03 ANO-INICIAL  PIC 9(04).
+         03 DIA-INICIAL      PIC 9(02).
+         03 MES-INICIAL      PIC 9(02).
+         03 ANO-INICIAL      PIC 9(04).
 
        01 DATA-FINAL.
-         03 DIA-FINAL  PIC 9(02).
-         03 MES-FINAL  PIC 9(02).
-         03 ANO-FINAL  PIC 9(04).
+         03 DIA-FINAL        PIC 9(02).
+         03 MES-FINAL        PIC 9(02).
+         03 ANO-FINAL        PIC 9(04).
 
-       01 DIA1          PIC 9(02).
-       01 DIA2          PIC 9(02).
+       01 DIA1               PIC 9(02).
+       01 DIA2               PIC 9(02).
 
        01 DIVIDE-ANO1.
          03 DIV1-POR-4       PIC 9(04).
@@ -44,16 +38,16 @@
          03 R2-400           PIC 9(04).
 
       *diferença de dias
-       01 DIFERENCA-DIAS PIC 9(05).
+       01 DIFERENCA-DIAS     PIC 9(05).
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
 
-            ADD 01 TO DIA-INICIAL.
-            ADD 01 TO MES-INICIAL.
+            ADD 01   TO DIA-INICIAL.
+            ADD 01   TO MES-INICIAL.
             ADD 2021 TO ANO-INICIAL.
 
-            ADD 27 TO DIA-FINAL.
-            ADD 01 TO MES-FINAL.
+            ADD 27   TO DIA-FINAL.
+            ADD 01   TO MES-FINAL.
             ADD 2025 TO ANO-FINAL.
       *JANEIRO
             IF (MES-INICIAL = 01)OR(MES-FINAL = 01)
@@ -62,7 +56,6 @@
             END-IF
       *FEVEREIRO
             IF (MES-INICIAL = 02)OR(MES-FINAL = 02)
-
 
              DISPLAY "QUANTIDADE DIAS: " DIA1
              DISPLAY "QUANTIDADE DIAS: " DIA2
@@ -74,29 +67,29 @@
               DIVIDE ANO-INICIAL BY 400  GIVING DIV1-POR-400
                REMAINDER R1-400
 
-              DIVIDE ANO-FINAL BY 4    GIVING DIV2-POR-4
+              DIVIDE ANO-FINAL   BY 4    GIVING DIV2-POR-4
                REMAINDER R2-4
-              DIVIDE ANO-FINAL BY 100  GIVING DIV2-POR-100
+              DIVIDE ANO-FINAL   BY 100  GIVING DIV2-POR-100
                REMAINDER R2-100
-              DIVIDE ANO-FINAL BY 400  GIVING DIV2-POR-400
+              DIVIDE ANO-FINAL   BY 400  GIVING DIV2-POR-400
                REMAINDER R2-400
 
               IF (R1-4 = 0)AND(R1-100 IS NOT = 0)OR(R1-400 = 0)
                DISPLAY "Ano1 " ANO-INICIAL " eh bissexto"
-                 ADD 29 TO DIA1
+                 ADD 29  TO DIA1
 
               ELSE
-                 ADD 28 TO DIA1
+                 ADD 28  TO DIA1
 
               END-IF
       *    Verifica SEGUNDO ANO
               IF (R2-4 = 0)AND(R2-100 IS NOT = 0)OR(R2-400 = 0)
                DISPLAY "Ano2 " ANO-FINAL " eh bissexto"
 
-                 ADD 29 TO DIA2
+                 ADD 29  TO DIA2
               ELSE
 
-                ADD 28 TO DIA2
+                ADD 28   TO DIA2
               END-IF
 
              DISPLAY "QUANTIDADE DIAS: " DIA1
@@ -154,14 +147,11 @@
               ADD 31 TO DIA2
             END-IF
 
-
             COMPUTE DIFERENCA-DIAS =(ANO-FINAL * 365)+(MES-FINAL * DIA2)
                                      + DIA-FINAL - (ANO-INICIAL * 365)-
                                      (MES-INICIAL * DIA1)- DIA-INICIAL.
 
             DISPLAY "A diferenca entre as datas eh: " DIFERENCA-DIAS.
-
-
 
             STOP RUN.
        END PROGRAM 04UNISYS.
